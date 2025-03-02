75393687b4dfa4cc6b3789ff0416d43239d74953
JeelTikiwala <jeeltik8@gmail.com>
2025-03-02 18:35:36 -0500

    netlify check

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-  site: 'https://jeeltikiwala.github.io',
-  base: '/PortfolioNew',
-  integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),  // Use the Netlify adapter for deployment
+  site: 'https://jeeltikiwala.github.io',  // Your site URL (change as needed)
+  base: '/PortfolioNew',  // Use a base path if your site is deployed in a subfolder
+  integrations: [tailwind(), icon()],  // Add any integrations you need
+  output: 'static',  // Build as a static site
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime],  // Add markdown plugins as needed
   },
 });
