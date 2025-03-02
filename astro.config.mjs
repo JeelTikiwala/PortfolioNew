2ea016e619141b10103da2bfe01eb202739a3e45
JeelTikiwala <jeeltik8@gmail.com>
2025-03-02 17:29:22 -0500

    added base again

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   output: 'server',
   adapter: netlify(),
   site: 'https://jeeltikiwala.github.io/',
-  // base: '/PortfolioNew',
+  base: '/PortfolioNew/',
   integrations: [tailwind(), icon()],
   output: "server",
   adapter: node({
