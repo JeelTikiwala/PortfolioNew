741073deffd4f45deb96d55f2c0df80e5c89310b
JeelTikiwala <jeeltik8@gmail.com>
2025-03-03 20:38:07 -0500

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,29 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import node from "@astrojs/node";
-import netlify from '@astrojs/netlify';
 import sitemap from "@astrojs/sitemap";
 
-// We are doing some URL setup here to handle your local and production URLs
 const SERVER_PORT = 3000;
-// the url to access your portfolio during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-// the url to access your portfolio after deploying it (e.g. Netlify)
-const LIVE_URL = "https://jeeltikiwala.github.io";
+const LIVE_URL = "https://jeeltikiwala.github.io";  // Change this to your GitHub Pages URL
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
 
-// When you're building your site, change the base URL to your live URL
 if (isBuild) {
   BASE_URL = LIVE_URL;
 }
 
 export default defineConfig({
   server: { port: SERVER_PORT },
-  site: BASE_URL,  // dynamic URL setup based on build or local development
-  base: '/PortfolioNew', // This should be the base path you want for your deployed site
+  site: BASE_URL,
+  base: '/PortfolioNew',  // Set base path to your GitHub repository name
   integrations: [
     tailwind(),
     icon(),
-    sitemap()  // Add sitemap integration if you need it
+    sitemap()
   ],
-  output: "server",
+  output: "static",  // Change output to 'static' for GitHub Pages
   adapter: node({
     mode: "standalone",
   }),
